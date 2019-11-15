70123e99b9472a58c2eae4399fb482d8ed5db685
Serg6669999 <schkurko@yandex.ru>
2019-11-15 11:04:19 +0300

    post-send

--- Alisa/use/views.py
+++ Alisa/use/views.py
@@ -1,17 +1,17 @@
 from django.utils.http import is_safe_url, urlunquote
 from django.shortcuts import render
+import requests
 
 
 def post(request):
 
     if request.method == "POST":
-
-       data = "post"
+        post_data = {'name': 'Да'}
+        response = requests.post('https://dialogs.yandex.ru/developer/skills/f8eb61f3-8faf-4180-a9f5-0cd2b76c52ff/draft/test', data=post_data)
+        content = response.content
+        word = "post"
 
     else:
-        data = "hello"
+        word = "hello"
 
-
-
-
-    return render(request, 'use/main.html', {'data': data})
+    return render(request, 'use/main.html', {'word': word})
