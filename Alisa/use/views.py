41a283be521a374ba6e438e550a1fd08f659dce6
Serg6669999 <schkurko@yandex.ru>
2019-11-15 09:57:50 +0300

    post-hello

--- Alisa/use/views.py
+++ Alisa/use/views.py
@@ -1,12 +1,17 @@
+from django.utils.http import is_safe_url, urlunquote
 from django.shortcuts import render
-from .api import main
+
 
 def post(request):
 
     if request.method == "POST":
+
        data = "post"
-       print(main())
+
     else:
         data = "hello"
+
+
+
 
     return render(request, 'use/main.html', {'data': data})
