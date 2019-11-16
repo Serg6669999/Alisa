951e4a1bfd00260a4a4f2b5f69bf43ab241374bd
Serg6669999 <schkurko@yandex.ru>
2019-11-16 23:27:03 +0300

    jsonrespons-alisa

--- Alisa/use/views.py
+++ Alisa/use/views.py
@@ -7,6 +7,18 @@ import requests
 
 def post(request):
 
+    text = {'text': 'Да'}
 
-    return JsonResponse({'foo': 'bar'})
+    end_session = {'end_session': 'false'}
+    session = request.POST.get("session")
+    session_id = session["session_id"]
+    message_id = session["message_id"]
+    user_id = session["user_id"]
+    version = {"1.0"}
+    response = {'response': text, 'end_session': end_session,
+                                      'session': {'session_id':session_id,
+                                      'message_id': message_id,
+                                      'user_id': user_id,
+                                      'version': version,}}
+    return JsonResponse(response)
 
