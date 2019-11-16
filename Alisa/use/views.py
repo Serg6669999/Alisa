0bc2d0dd401479994bd55d523a11a6e126b55202
Serg6669999 <schkurko@yandex.ru>
2019-11-16 20:20:22 +0300

    jsonrespons

--- Alisa/use/views.py
+++ Alisa/use/views.py
@@ -1,5 +1,6 @@
 from django.utils.http import is_safe_url, urlunquote
 from django.shortcuts import render
+from django.http import JsonResponse
 from .api import main
 import requests
 
@@ -7,11 +8,7 @@ import requests
 def post(request):
 
     if request.method == "POST":
-        post_data = {'name': 'Да'}
-        response = requests.post('https://dialogs.yandex.ru/developer/skills/f8eb61f3-8faf-4180-a9f5-0cd2b76c52ff/draft/test', data=post_data)
-        content = response.content
-
-
+        return JsonResponse({'foo': 'bar'})
         word = "post"
 
     else:
