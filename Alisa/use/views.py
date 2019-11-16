b71a1000f055aba470077ad2cfe2e00784b601dd
Serg6669999 <schkurko@yandex.ru>
2019-11-16 13:45:15 +0300

    flask

--- Alisa/use/views.py
+++ Alisa/use/views.py
@@ -1,14 +1,16 @@
 from django.utils.http import is_safe_url, urlunquote
 from django.shortcuts import render
+from Alisa.use.api import main
 import requests
 
 
 def post(request):
 
     if request.method == "POST":
-        post_data = {'name': 'Да'}
-        response = requests.post('https://dialogs.yandex.ru/developer/skills/f8eb61f3-8faf-4180-a9f5-0cd2b76c52ff/draft/test', data=post_data)
-        content = response.content
+        #post_data = {'name': 'Да'}
+        #response = requests.post('https://dialogs.yandex.ru/developer/skills/f8eb61f3-8faf-4180-a9f5-0cd2b76c52ff/draft/test', data=post_data)
+        #content = response.content
+        main()
         word = "post"
 
     else:
